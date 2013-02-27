3fb81b9097839db93c7f716005eb8961fa637342
Laki <glakomy@gmail.com>
2013-02-26 22:41:37 -0600

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,14 +60,12 @@ describe "Authentication" do
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
         end
 
         describe "in the Microposts controller" do
-
           describe "submitting to the create action" do
             before { post microposts_path }
             specify { response.should redirect_to(signin_path) }
@@ -77,6 +75,18 @@ describe "Authentication" do
             before { delete micropost_path(FactoryGirl.create(:micropost)) }
             specify { response.should redirect_to(signin_path) }
           end
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
         end
       end
 
@@ -94,6 +104,16 @@ describe "Authentication" do
 
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
       end
